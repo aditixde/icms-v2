86ccb6d37e34879daf5ea7bce629587c9a486523
aditi <40533629+aditixde@users.noreply.github.com>
2025-11-04 01:51:06 +0530

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import EquilibriumFinder from './components/EquilibriumFinder';
-import { ExplanationPage } from './components/ExplanationPage';
 import { ArchetypesPage } from './components/ArchetypesPage';
 import { ArchetypeDetailsPage } from './components/ArchetypeDetailsPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<EquilibriumFinder />} />
-      <Route path="/explanation" element={<ExplanationPage />} />
-      <Route path="/archetypes" element={<ArchetypesPage />} />
+      <Route path="/" element={<ArchetypesPage />} />
       <Route path="/archetype-details" element={<ArchetypeDetailsPage />} />
     </Routes>
   );
